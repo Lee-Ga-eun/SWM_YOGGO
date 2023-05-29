dc1b97c029ddff9ac421e620aa6124739dd438e3
Gaeun Lee <gaeun0800@gmail.com>
2023-05-30 02:08:39 +0900

    app Bar 코드 분리

--- yoggo/lib/home_screen.dart
+++ yoggo/lib/home_screen.dart
@@ -6,13 +6,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white, // 전체 컬러
       appBar: AppBar(
         elevation: 0, // 음영 제거
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.yellow, // App Bar 컬러지정
         title: const Text(
           "YOGGO",
           style: TextStyle(
             color: Colors.black,
+            //fontWeight: FontWeight.bold,
           ),
         ),
       ),
--- yoggo/lib/main.dart
+++ yoggo/lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import './home_screen.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   // 기초적인 위젯이며 화면에 띄어주는 역할 정도만 한다
   //build 메소드 필요
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-              title: Text('Hello YOGGO Bar!'),
-              ),
-          body: Center(
-            child: Text('Hello world'),
-          )),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
