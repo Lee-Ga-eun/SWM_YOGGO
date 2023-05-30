811a701a55e250ca570fc7cc521c1b85937111e6
Gaeun Lee <gaeun0800@gmail.com>
2023-05-30 13:44:13 +0900

    AppBar: Home, Point 추가

--- yoggo/lib/home_screen.dart
+++ yoggo/lib/home_screen.dart
@@ -3,20 +3,45 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  void pointFunction() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white, // 전체 컬러
       appBar: AppBar(
+        // actions: 오른쪽 사이드바, leading: 왼쪽 사이드바
         elevation: 0, // 음영 제거
         backgroundColor: Colors.yellow, // App Bar 컬러지정
         title: const Text(
           "YOGGO",
           style: TextStyle(
-            color: Colors.black,
-            //fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
         ),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: IconButton(
+            icon: const Icon(Icons.money_rounded),
+            onPressed: pointFunction,
+            iconSize: 40,
+            color: Colors.white,
+          ),
+        ),
+
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: IconButton(
+              onPressed: pointFunction,
+              icon: const Icon(
+                Icons.home,
+                size: 30,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
