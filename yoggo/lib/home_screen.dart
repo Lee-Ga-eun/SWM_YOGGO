9fc5a2e2ab45adadcdb4af41511f3f99203f88f9
Gaeun Lee <gaeun0800@gmail.com>
2023-06-04 17:22:56 +0900

    Update home_screen.dart

--- yoggo/lib/home_screen.dart
+++ yoggo/lib/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:yoggo/models/webtoon.dart';
+import 'package:yoggo/screens/detail_screens.dart';
+import 'package:yoggo/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
-  void pointFunction() {}
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+  void pointFunction() {} // AppBar 아이콘클릭
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +17,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         // actions: 오른쪽 사이드바, leading: 왼쪽 사이드바
         elevation: 0, // 음영 제거
-        backgroundColor: Colors.yellow, // App Bar 컬러지정
+        backgroundColor: const Color(0xFFE2DDEB), // App Bar 컬러지정
         title: const Text(
           "YOGGO",
           style: TextStyle(
@@ -43,6 +48,73 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
+      body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              //Listview: 많은 양을 연속적으로 보여주고 싶을 때 row, column비추.
+              return Column(
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Expanded(
+                    child: ListView.separated(
+                      // ListView는 자동으로 스크롤뷰를 가져와줌
+                      // ListView.builder는 메모리 낭비하지 않게 해줌(사용자가 스크롤 할 때 데이터 로딩)
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        // 사용자가 보고 있지 않다면 메모리에서 삭제
+                        var webtoon = snapshot.data![index];
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailScreens(
+                                      title: webtoon.title,
+                                      thumb: webtoon.thumb,
+                                      id: webtoon.id),
+                                ));
+                          },
+                          child: Column(
+                            children: [
+                              Hero(
+                                tag: webtoon.id,
+                                child: Container(
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  height: 200,
+                                  child: Image.network(
+                                    webtoon.thumb,
+                                    headers: const {
+                                      "User-Agent":
+                                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                                    },
+                                  ),
+                                ),
+                              ),
+                              Text(webtoon.title),
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(width: 20),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.orange,
+                ),
+              );
+            }
+          }),
     );
   }
 }
