85c57f6abc2938992f47e70d8cd94b4ec0873e02
Gaeun Lee <gaeun0800@gmail.com>
2023-06-05 16:58:59 +0900

    Update intro.dart

--- yoggo/lib/widgets/intro.dart
+++ yoggo/lib/widgets/intro.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 // class SplashScreen extends StatelessWidget {
 //   const SplashScreen({super.key});
@@ -8,62 +7,66 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  static var splashUp = true;
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    print('초기화');
-    initialization();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   print('초기화');
+  //   initialization();
+  // }
 
-  void initialization() async {
-    // This is where you can initialize the resources needed by your app while
-    // the splash screen is displayed.  Remove the following example because
-    // delaying the user experience is a bad design practice!
-    // ignore_for_file: avoid_print
-    print('ready in 3...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 2...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 1...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('go!');
-    FlutterNativeSplash.remove();
-  }
+  // void initialization() async {
+  //   // print('ready in 3...');
+  //   await Future.delayed(const Duration(seconds: 3));
+  //   print('go!');
+  //   FlutterNativeSplash.remove();
+  //   SplashScreen.splashUp = false;
+  //   print(SplashScreen.splashUp);
+  //}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow, // Set the background color to yellow
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'YOGGO',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16),
-            AnimatedTextKit(
-              animatedTexts: [
-                TypewriterAnimatedText(
-                  'Welcome to YOGGO!',
-                  textStyle: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  speed: const Duration(milliseconds: 200),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/images/bkground.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'YOGGO',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 16),
+              AnimatedTextKit(
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                    'Welcome to YOGGO!',
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    speed: const Duration(milliseconds: 200),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
