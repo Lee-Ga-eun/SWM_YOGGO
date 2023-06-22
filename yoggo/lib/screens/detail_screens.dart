5c8a3dcad1fac45f18d95e79ed20a7bdec59e034
Gaeun Lee <gaeun0800@gmail.com>
2023-06-22 14:41:55 +0900

    책 선택 시 책의 이미지들 캐시에 저장

--- yoggo/lib/screens/detail_screens.dart
+++ yoggo/lib/screens/detail_screens.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yoggo/main.dart';
 import '../screens/reader.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:yoggo/size_config.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class DetailScreens extends StatefulWidget {
   final String title, thumb, summary;
@@ -29,6 +31,8 @@ class _DetailScreensState extends State<DetailScreens> {
   List<dynamic> voices = [];
   int cvi = 0;
   bool canChanged = false;
+  int lastPage = 0;
+  int contentId = 1;
 
   Future<void> fetchPageData() async {
     final url = 'https://yoggo-server.fly.dev/content/${widget.id}';
@@ -43,6 +47,8 @@ class _DetailScreensState extends State<DetailScreens> {
         print('voiceName');
         //print(contentText);
         voices = data['voice'];
+        lastPage = data['last'];
+        contentId = data['contentId'];
         print(voices);
         for (var voice in voices) {
           print(voice['voiceName']);
@@ -52,6 +58,14 @@ class _DetailScreensState extends State<DetailScreens> {
           voiceId = data['voice'][0]['contentVoiceId'];
         });
       } else {}
+    }
+  }
+
+  Future<void> precacheImages(BuildContext context) async {
+    for (int i = 1; i <= lastPage; i++) {
+      final imageUrl = '$contentUrl$contentId-${i.toString()}.png';
+      await precacheImage(CachedNetworkImageProvider(imageUrl), context);
+      print(imageUrl);
     }
   }
 
@@ -76,6 +90,7 @@ class _DetailScreensState extends State<DetailScreens> {
 
   @override
   Widget build(BuildContext context) {
+    precacheImages(context);
     SizeConfig().init(context);
     return Scaffold(
       backgroundColor: const Color(0xFFF1ECC9).withOpacity(1),
@@ -223,98 +238,12 @@ class _DetailScreensState extends State<DetailScreens> {
                           SizedBox(
                             height: SizeConfig.defaultSize!,
                           ),
-                          // --------------------피그마 아이콘이랑 일치하는 것:: contentVoiceId 동적 기능 없음 ------------
-                          /*GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              isClicked = !isClicked; // 클릭 상태를 토글합니다.
-                            });
-                          },
-                          child: Container(
-                            alignment: Alignment.centerLeft,
-                            child: Container(
-                              child: Image.network(
-                                'https://media.discordapp.net/attachments/1114865651312508958/1115512272987623484/actor_kelly.png',
-                                width: 30,
-                                color: isClicked
-                                    ? const Color.fromARGB(255, 255, 66, 129)
-                                    : null,
-                              ),
-                            ),
-                          ),
-                        ),*/
-                          // ----------------성우 리스트 수에 따라 아이콘 생성 시작-------------------
-                          // Row(
-                          //   children: voices.map((voice) {
-                          //     bool isClicked = (cvi == voice['contentVoiceId']);
-                          //     //for (var voice in voices)
-                          //     return GestureDetector(
-                          //       onTap: () {
-                          //         cvi = voice[
-                          //             'contentVoiceId']; // 1, 2, 3 등 --> 이 값을 밑에 화살표 부분에 넘겨준 것
-                          //         setState(() {
-                          //           isClicked = !isClicked; // 클릭 상태
-                          //         });
-                          //       },
-                          //       child: Column(
-                          //         children: [
-                          //           Image.network(
-                          //             _getImageForVoice(voice['voiceName']),
-                          //             //Icons.person, // Icons.person은 기본 제공되는 아이콘이다. 이걸 그림으로 바꾸려면 Icon()을 지우고
-                          //             //Image.network('https://media.discordapp.net/attachments/1114865651312508958/1115512272987623484/actor_kelly.png',) 이렇게 처리를 해줘야 한다
-                          //             color: isClicked
-                          //                 ? const Color.fromARGB(
-                          //                     // 선택하면 색이 바껴야 하는데 전부 다 바껴서 문제
-                          //                     255,
-                          //                     255,
-                          //                     66,
-                          //                     129)
-                          //                 : null,
-                          //           ),
-                          //           const SizedBox(height: 8),
-                          //           Text(voice['voiceName']),
-                          //         ],
-                          //       ),
-                          //     );
-                          //   }).toList(),
-                          // )
-                          // --------------------성우 아이콘 배치 완료  ---------
                         ]),
                       ),
                     ),
                   ],
                 ),
               ),
-              // Expanded(
-              //   flex: 2,
-              //   child: Container(
-              //     color: Colors.blue,
-              //     // margin: const EdgeInsets.only(right: 50, bottom: 100),
-
-              //     alignment: Alignment.topRight,
-              //     //child: Padding(
-              //     //padding: const EdgeInsets.only(right: 30, bottom: 100),
-              //     child: GestureDetector(
-              //       onTap: () {
-              //         Navigator.push(
-              //           context,
-              //           MaterialPageRoute(
-              //             builder: (context) => FairytalePage(
-              //               // 다음 화면으로 contetnVoiceId를 가지고 이동
-              //               voiceId: cvi,
-              //             ),
-              //           ),
-              //         );
-              //       },
-              //       child: const Icon(
-              //         Icons.arrow_circle_right_outlined,
-              //         size: 50,
-              //         color: Colors.white,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-//추가
               Expanded(
                 flex: 2,
                 child: Row(children: [
@@ -329,49 +258,6 @@ class _DetailScreensState extends State<DetailScreens> {
                       // 썸네일 사진
                       flex: 1,
                       child: Container()
-                      // Container(
-                      //   //  color: Colors.pink,
-                      //   child: Row(
-                      //     mainAxisAlignment: MainAxisAlignment.center,
-                      //     children: voices.map((voice) {
-                      //       bool isClicked = (cvi == voice['contentVoiceId']);
-                      //       //for (var voice in voices)
-                      //       return GestureDetector(
-                      //         onTap: () {
-                      //           cvi = voice[
-                      //               'contentVoiceId']; // 1, 2, 3 등 --> 이 값을 밑에 화살표 부분에 넘겨준 것
-                      //           setState(() {
-                      //             isClicked = !isClicked; // 클릭 상태
-                      //           });
-                      //         },
-                      //         child: Column(
-                      //           children: [
-                      //             Image.network(
-                      //               _getImageForVoice(
-                      //                 voice['voiceName'],
-                      //               ),
-                      //               //Icons.person, // Icons.person은 기본 제공되는 아이콘이다. 이걸 그림으로 바꾸려면 Icon()을 지우고
-                      //               //Image.network('https://media.discordapp.net/attachments/1114865651312508958/1115512272987623484/actor_kelly.png',) 이렇게 처리를 해줘야 한다
-                      //               color: isClicked
-                      //                   ? const Color.fromARGB(
-                      //                       // 선택하면 색이 바껴야 하는데 전부 다 바껴서 문제
-                      //                       255,
-                      //                       255,
-                      //                       66,
-                      //                       129)
-                      //                   : null,
-                      //               height: 50,
-                      //             ),
-                      //             // const SizedBox(height: 8),
-                      //             Text(voice['voiceName']),
-                      //           ],
-                      //         ),
-                      //       );
-                      //     }).toList(),
-                      //   ),
-                      // )
-
-                      //color: Colors.green,),
                       ),
                   Expanded(
                     // 제목과 책 내용 요약
