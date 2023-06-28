c21e534c2b87a8c99c9192433bcbca58f8122a1a
Gaeun Lee <gaeun0800@gmail.com>
2023-06-29 01:19:30 +0900

    Create login_screen.dart

---
+++ yoggo/lib/login_screen.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:yoggo/home_screen.dart';
+import 'package:yoggo/models/user.dart';
+import 'package:yoggo/size_config.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Future<void> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser != null) {
+      UserModel user = UserModel(
+        name: googleUser.displayName!,
+        email: googleUser.email,
+        providerId: googleUser.id,
+        provider: 'google',
+      );
+
+      var url = Uri.parse('https://yoggo-server.fly.dev/auth/login');
+      print(json.encode(user.toJson()));
+      var response = await http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode(user.toJson()));
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        // 로그인 성공
+        var responseData = json.decode(response.body);
+        var token = responseData['token'];
+        var prefs = await SharedPreferences.getInstance();
+        await prefs.setString('token', token);
+        await Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()));
+      } else {
+        // 로그인 실패
+        print('로그인 실패. 상태 코드: ${response.statusCode}');
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: Colors.yellow, // Set the background color to yellow
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'lib/images/bkground.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            top: 20 * SizeConfig.defaultSize!,
+            left: MediaQuery.of(context).size.width / 2 - 95,
+            child: GestureDetector(
+              onTap: signInWithGoogle,
+              child: Image.asset(
+                'lib/images/google_login.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
