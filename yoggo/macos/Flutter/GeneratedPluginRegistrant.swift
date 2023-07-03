1942da8e02ce6f41d356a799cf71c1d494d8b97f
Gaeun Lee <gaeun0800@gmail.com>
2023-07-03 22:10:24 +0900

    record

--- yoggo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ yoggo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_core
 import in_app_purchase_storekit
 import path_provider_foundation
 import record_macos
+
 import shared_preferences_foundation
 import sign_in_with_apple
 import sqflite
@@ -24,7 +25,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
+    RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
+
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
