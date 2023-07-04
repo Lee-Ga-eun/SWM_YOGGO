f34f06b42851f368ec18104bb8e5dbab2d7f7af3
GyeongAh Kang <acvxxx@korea.ac.kr>
2023-07-04 23:53:09 +0900

    Feat: Purchase

--- yoggo/lib/component/login_screen.dart
+++ yoggo/lib/component/login_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:yoggo/component/home_screen.dart';
 import 'package:yoggo/models/user.dart';
 import 'package:yoggo/size_config.dart';
@@ -15,6 +18,20 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String generateNonce([int length = 32]) {
+    final charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
   Future<void> signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -46,6 +63,46 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  Future<void> signInWithApple() async {
+    // Trigger the authentication flow
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: nonce,
+    );
+    print(appleCredential);
+    if (appleCredential != null) {
+      // UserModel user = UserModel(
+      //   name: appleCredential.email!,
+      //   email: appleCredential.fullName,
+      //   providerId: appleCredential.id,
+      //   provider: 'apple',
+      // );
+      // var url = Uri.parse('https://yoggo-server.fly.dev/auth/login');
+      // print(json.encode(user.toJson()));
+      // var response = await http.post(url,
+      //     headers: {'Content-Type': 'application/json'},
+      //     body: json.encode(user.toJson()));
+
+      // if (response.statusCode == 200 || response.statusCode == 201) {
+      //   // 로그인 성공
+      //   var responseData = json.decode(response.body);
+      //   var token = responseData['token'];
+      //   var prefs = await SharedPreferences.getInstance();
+      //   await prefs.setString('token', token);
+      //   await Navigator.push(context,
+      //       MaterialPageRoute(builder: (context) => const HomeScreen()));
+      // } else {
+      //   // 로그인 실패
+      //   print('로그인 실패. 상태 코드: ${response.statusCode}');
+      // }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -71,6 +128,16 @@ class _LoginScreenState extends State<LoginScreen> {
               onTap: signInWithGoogle,
               child: Image.asset(
                 'lib/images/google_login.png',
+              ),
+            ),
+          ),
+          Positioned(
+            top: 30 * SizeConfig.defaultSize!,
+            left: MediaQuery.of(context).size.width / 2 - 95,
+            child: GestureDetector(
+              onTap: signInWithApple,
+              child: Image.asset(
+                'lib/images/apple_login.png',
               ),
             ),
           ),
