9e716b92a1a80354b9d474a53c6473b4960f1d34
Gaeun Lee <62734844+Lee-Ga-eun@users.noreply.github.com>
2023-07-26 18:18:46 +0900

    푸시 기능 | 녹음 플로우 변경

--- yoggo/lib/component/home/view/home_screen.dart
+++ yoggo/lib/component/home/view/home_screen.dart
@@ -16,6 +16,8 @@ import '../viewModel/home_screen_cubit.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import '../../globalCubit/user/user_cubit.dart';
+import '../../push.dart';
+import 'package:yoggo/main.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -588,6 +590,7 @@ class DataList extends StatelessWidget {
     return BlocBuilder<DataCubit, List<BookModel>>(
       builder: (context, state) {
         if (state.isEmpty) {
+          showNotification(flutterLocalNotificationsPlugin);
           return Center(
             child: Center(
               child: LoadingAnimationWidget.fourRotatingDots(
