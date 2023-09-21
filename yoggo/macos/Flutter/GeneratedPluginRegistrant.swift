fca85fc2f88ab401c386cbf3f2a739125f6341c2
Gyeongah Kang <acvxxx@korea.ac.kr>
2023-09-21 23:44:12 +0900

    Feat: invite friends & request review

--- yoggo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ yoggo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,9 +11,11 @@ import firebase_analytics
 import firebase_auth
 import firebase_core
 import in_app_purchase_storekit
+import in_app_review
 import path_provider_foundation
 import purchases_flutter
 import record_macos
+import share_plus
 import shared_preferences_foundation
 import sign_in_with_apple
 import sqflite
@@ -26,9 +28,11 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
